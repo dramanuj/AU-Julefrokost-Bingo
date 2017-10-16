75f796b27dc5d5703afa31922c99d7c18b407bb5
Dev <dramanuj@users.noreply.github.com>
2017-10-16 16:15:28 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,69 @@
+
+var headerText = "AU Julefrokost Bingo";
+
+var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>modified from jeffehobbs original code</a>";
+
+var winText = "Full Row or Column";
+
+var fullWinText = "Full House";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var boolWinSndPlayed = false;
+
+var winSnd = new Audio("audio/win.mp3");
+
+
+var JSONBingo = {"squares": [
+        {"square": "1"},
+        {"square": "2"},
+        {"square": "3"},
+        {"square": "4"},
+        {"square": "5"},
+        {"square": "6"},
+        {"square": "7"},
+        {"square": "8"},
+        {"square": "9"},
+        {"square": "10"},
+        {"square": "11"},
+        {"square": "12"},
+        {"square": "13"},
+        {"square": "14"},
+        {"square": "15"},
+        {"square": "16"},
+        {"square": "17"},
+        {"square": "18"},
+        {"square": "19"},
+        {"square": "20"},
+        {"square": "21"},
+        {"square": "22"},
+        {"square": "23"},
+        {"square": "24"},
+        {"square": "25"},
+        {"square": "26"},
+        {"square": "27"},
+        {"square": "28"},
+        {"square": "29"},
+        {"square": "30"},
+        {"square": "31"},
+        {"square": "32"},
+        {"square": "33"},
+        {"square": "34"},
+        {"square": "35"},
+        {"square": "36"},
+        {"square": "37"},
+        {"square": "38"},
+        {"square": "39"},
+        {"square": "40"},
+        {"square": "41"},
+        {"square": "42"},
+        {"square": "43"},
+        {"square": "44"},
+        {"square": "45"},
+        {"square": "46"},
+        {"square": "47"},
+        {"square": "48"},
+        {"square": "49"},
+        {"square": "50"}
+    ]
+};
